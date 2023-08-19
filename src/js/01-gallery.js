79ecab020bfc088b3fab5cb7adb581aa6ce54e2e
Dmytro Sayko <saykodmytro@icloud.com>
2023-08-19 19:09:08 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.innerHTML = createGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
