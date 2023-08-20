caec9d8034728700d8abb43183a003712bdf4931
Dmytro Sayko <saykodmytro@icloud.com>
2023-08-20 16:55:24 +0300

    up

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,1 @@
-// localStorage.setItem('TEST_KEY', JSON.stringify([1, 2, 3, 4]));
-// // localStorage.setItem('TEST_KEY_1', '[1, 2, 3, 4]');
-// localStorage.setItem('TEST_KEY_1', JSON.stringify({ name: 'hello' }));
-// console.log(JSON.parse(localStorage.getItem('TEST_KEY_1')));
-// // localStorage.removeItem('TEST_KEY_1');
-// localStorage.clear();
-
-const local = document.querySelector('.js-local');
-const session = document.querySelector('.js-session');
-
-local.addEventListener('click', () => {
-  localStorage.setItem('local', 'test local');
-});
-
-session.addEventListener('click', () => {
-  sessionStorage.setItem('session', 'test session');
-});
+console.log('Hello');
