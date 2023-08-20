b718a5456048d27a3daf8005be18947988146b3e
saykodmytro <64913341+saykodmytro@users.noreply.github.com>
2023-08-20 13:50:43 +0000

    Deploying to gh-pages from @ saykodmytro/goit-js-hw-08@3b779e2367e8f61a38a681ea1b39c6aa61863e79 🚀

---
+++ 02-video.20349071.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".js-local"),s=document.querySelector(".js-session");e.addEventListener("click",(()=>{localStorage.setItem("local","test local")})),s.addEventListener("click",(()=>{sessionStorage.setItem("session","test session")}));
+//# sourceMappingURL=02-video.20349071.js.map
---
+++ 02-video.c200bd9f.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".js-local"),t=document.querySelector(".js-session");e.addEventListener("click",(function(){localStorage.setItem("local","test local")})),t.addEventListener("click",(function(){sessionStorage.setItem("session","test session")}))}();
+//# sourceMappingURL=02-video.c200bd9f.js.map
