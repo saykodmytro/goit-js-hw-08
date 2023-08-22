7f29df1eefe9591be45ed8e133c0c32967cdf519
Dmytro Sayko <saykodmytro@icloud.com>
2023-08-23 00:58:00 +0300

    up

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('textarea'),
+};
 
+refs.form.addEventListener('sabmit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(evt) {}
+
+function onTextareaInput(evt) {
+  const value = evt.currentTurget.value;
+  console.log(value);
+}
