9b38c536039c1022491ef34893503563c262a3cf
Dmytro Sayko <saykodmytro@icloud.com>
2023-08-22 12:02:34 +0300

    Task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
-console.log('Hello');
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME_DURATION = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(TIME_DURATION, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem(TIME_DURATION);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
