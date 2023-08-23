31e725b1db5946616a74d3648adf5010927e06fe
saykodmytro <64913341+saykodmytro@users.noreply.github.com>
2023-08-23 10:19:08 +0000

    Deploying to gh-pages from @ saykodmytro/goit-js-hw-08@8bfa5892e448b2622712f4c37831ceeb2bf751c5 🚀

--- 03-feedback.6a521c59.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".feedback-form"),textarea:document.querySelector("textarea")};e.form.addEventListener("sabmit",(function(e){})),e.textarea.addEventListener("input",(function(e){var t=e.currentTurget.value;console.log(t)}))}();
-//# sourceMappingURL=03-feedback.6a521c59.js.map
