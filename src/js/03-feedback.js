8bfa5892e448b2622712f4c37831ceeb2bf751c5
Dmytro Sayko <saykodmytro@icloud.com>
2023-08-23 13:18:31 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,42 @@
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('textarea'),
-};
+import throttle from 'lodash.throttle';
 
-refs.form.addEventListener('sabmit', onFormSubmit);
-refs.textarea.addEventListener('input', onTextareaInput);
+const formRefs = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
 
-function onFormSubmit(evt) {}
+const formData = {};
 
-function onTextareaInput(evt) {
-  const value = evt.currentTurget.value;
-  console.log(value);
+onReloadPage();
+
+formRefs.addEventListener('input', throttle(onInputForm, 500));
+formRefs.addEventListener('submit', onSubmitForm);
+
+function onInputForm(evt) {
+  formData[evt.target.name] = evt.target.value;
+
+  const formDataJSON = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, formDataJSON);
+}
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+
+  if (formRefs.email.value === '' || formRefs.message.value === '') {
+    alert('FORM FIELDS MUST BE COMPLETED !!!');
+    return;
+  }
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
+
+function onReloadPage() {
+  const jsontLocalStorage = localStorage.getItem(STORAGE_KEY);
+
+  if (jsontLocalStorage) {
+    const parseLocalStorage = JSON.parse(jsontLocalStorage);
+    const { email, message } = parseLocalStorage;
+
+    formRefs.email.value = email;
+    formRefs.message.value = message;
+  }
 }
